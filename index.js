77de65ea5367307f32d8dc72e3d57114c49ae2fd
Erick11391 <xandererik4@gmail.com>
2024-12-02 21:26:26 +0300

    modified

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 
-function distanceFromHqInBlocks(street) {
-    const hq = 42; 
-    return Math.abs(street - hq); 
-  }
-  
+function distanceFromHqInBlocks(location) {
+  const hq = 42;
+  return Math.abs(location - hq);
+}
+function distanceFromHqInFeet(location) {
+  const blocks = distanceFromHqInBlocks(location);
+  return blocks * 264; 
+}
 
-function distanceFromHqInFeet(street) {
-    const feetPerBlock = 264;
-    return distanceFromHqInBlocks(street) * feetPerBlock; 
-  }
-  
+function distanceTravelledInFeet(start, destination) {
+  const blocksTravelled = Math.abs(destination - start);
+  return blocksTravelled * 264;
+}
 
-  function distanceTravelledInFeet(start, destination) {
-    const feetPerBlock = 264; 
-    return Math.abs(destination - start) * feetPerBlock;
-  }
-  
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = distanceTravelledInFeet(start, destination);
 
-  function calculatesFarePrice(start, destination) {
-    const distanceInFeet = distanceTravelledInFeet(start, destination);
-  
-    if (distanceInFeet <= 400) {
+  if (distanceInFeet <= 400) {
       return 0;
-    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+  } else if (distanceInFeet <= 2000) {
       return (distanceInFeet - 400) * 0.02; 
-    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+  } else if (distanceInFeet <= 2500) {
       return 25; 
-    } else {
-      return 'cannot travel that far';
-    }
+  } else {
+      return "cannot travel that far"; 
   }
-  
- 
-  
+}
+
+console.log(distanceFromHqInBlocks(43)); 
+console.log(distanceFromHqInFeet(43));  
+console.log(distanceTravelledInFeet(34, 32)); 
+console.log(calculatesFarePrice(34, 32)); 
